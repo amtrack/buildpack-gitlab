abbd6b1c70de661a1365646dfeccc8c24ad5ce02
Matthias Rolke <mr.amtrack@gmail.com>
2014-06-15 13:52:07 +0200

    move functionality of .ssh/rc into gitlab2gitlabshell-env.sh

--- support/templates/gitlab2gitlabshell-env.sh
+++ support/templates/gitlab2gitlabshell-env.sh
@@ -2,4 +2,6 @@
 if [ -f /app/.profile.d/app-env.sh ]; then
 	test -d /home/git/.ssh || mkdir -p /home/git/.ssh
 	sed "s/export //" /app/.profile.d/app-env.sh > /home/git/.ssh/environment
+	. /home/git/.ssh/environment
+	echo $REDIS_URL > /home/git/.ssh/REDIS_URL
 fi
